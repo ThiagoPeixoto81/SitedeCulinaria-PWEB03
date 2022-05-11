ede150611f0b8f86c9a71ce1a1d0f05f5abe06c2
Thiago Peixoto <thiagopeixoto81xd@gmail.com>
2022-05-11 17:01:14 -0300

    Correções

--- scripts/verify.js
+++ scripts/verify.js
@@ -5,19 +5,19 @@ function validar(){
 
     if (input_name.value == ""){
         alert("Nome está vazio");
-
         input_name.focus();
-
         return;
+
     }else if (input_email.value == ""){
         alert("Insira seu email");
-
         input_email.focus();
         return;
+
     }else if (input_senha.value == ""){
         alert("insira sua senha");
         input_senha.focus();
         return;
+        
     }else{
         window.location.href = "./redirect.html"
     }
